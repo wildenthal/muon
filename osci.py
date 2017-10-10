81c21ac3951f1c1cc475e59dafe37d2e0e49cf3d
wildenthal <8503469+wildenthal@users.noreply.github.com>
2017-10-10 16:26:14 -0300

    ahora si agrega .py que barre thresholds y mide nro de eventos

---
+++ osci.py
@@ -0,0 +1,37 @@
+import visa
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+rm = visa.ResourceManager('@py')
+osci = rm.open_resource(rm.list_resources()[0])
+
+voltajeinicial = float(input('Threshold más grande: '))
+voltajefinal = float(input('Threshold menos grande: '))
+numerothresholds = int(input('Decime cuántos datos: '))
+segundos = int(input('Cuánto tiempo mido? Decimeee: '))
+print('Hora de detectar muones. Que la fuerza electrodébil te acompanie.')
+listathresholds = np.linspace(-.1,-.08,3)
+listathresholds = np.linspace(voltajeinicial,voltajefinal,num=numerothresholds)
+
+eventos = np.zeros(len(listathresholds))
+for index,threshold in enumerate(listathresholds):
+    osci.write('trigger:a:level:ch1 {}'.format(threshold)) #apago la adquisición y cambio el trigger
+    escala = float(osci.query('horizontal:scale?')) #esto se fija qué escala estamos usando
+    osci.write('horizontal:scale {}; scale {}'.format(1,escala)) #esto reinicia el numero de adquisiciones
+    time.sleep(segundos)
+    eventos[index] = int(osci.query('acquire:numacq?'))
+np.savetxt('medicion_{}.csv'.format(time.strftime("%m-%d-%H%M")),np.transpose([listathresholds,eventos]),delimiter=',')
+plt.plot(-1*listathresholds,eventos,'ro')
+plt.grid()
+plt.show()
+osci.close()
+
+####
+#código viejo:
+####
+#seg = int(input('Cuántos segundos? '))
+#muonesIniciales = int(osci.query('ACQuire:NUMACq?'))
+#time.sleep(seg)
+#muonesNuevos = int(osci.query('ACQuire:NUMACq?')) - muonesIniciales
+#print('En {}s llegaron {} muones :u'.format(seg,muonesNuevos))
+#osci.close()
