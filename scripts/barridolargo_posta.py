0224c7ed7280c2649338eb46cac1a08ebf08b95f
wildenthal <8503469+wildenthal@users.noreply.github.com>
2017-11-02 22:02:27 -0300

    Agrega barridolargo_posta versión básica
    Esta versión trabaja con las curvas ya levantadas pero en el futuro podría agregarse que haga un nuevo barrido, interpole, y trabaje con esos parámetros

--- scripts/barridolargo_posta.py
+++ scripts/barridolargo_posta.py
@@ -1,22 +1,48 @@
 import visa
 import time
 import numpy as np
-rm = visa.ResourceManager('@py')
-osci = rm.open_resource(rm.list_resources()[0],read_termination='\n')
+import sys
 
-#barrido de flujo
-divisiones = 24*6
-tiempototal = 86400
-muones = []
+#El programa debe ser llamado de la siguiente manera:
+# >>sudo python3 barridolargo.py rateminimo ratemaximo nropuntos tiempoporpunto
+#donde el tiempo esta medido en segundos
+#rate debe ser mayor que 22
 
 dateinicial = time.strftime("%y.%m.%d_%H.%M")
-tiempocero = time.time()
-for hora in range(divisiones):
+rm = visa.ResourceManager('@sim')
+osci = rm.open_resource(rm.list_resources()[0],read_termination='\n')
+
+#Parametros de nuestra medicion
+nropuntos = int(sys.argv[3])
+ratelist = np.linspace(float(sys.argv[1]), float(sys.argv[2]), int(sys.argv[3]))
+tiempomedicion = float(sys.argv[4]) #Cuanto tiempo quiero barrer cada threshold
+
+#Funcion que calcula el treshold para cada PMT en funcion del rate deseado
+def thresholds(rate):
+    y2 = -.041866
+    A2 = 122846.14004
+    R2 = 134.897872
+    y1 = 21.27975
+    A1 = 783197.61244
+    R1 = 97.72976
+    threshold1 = np.log((rate - y1)/A1)/R1 #rate debe ser mayor que 22
+    threshold2 = np.log((rate - y2)/A2)/R2 
+    return threshold1, threshold2
+
+#Crea csv para guardar
+matrizdedatos = np.transpose([ratelist,np.zeros(nropuntos)])
+
+#Hace el barrido
+for index,rate in enumerate(ratelist):
+    threshold1, threshold2 = thresholds(rate)
+    osci.write('TRIGger:A:SETHold:CLOCk:THReshold {}'.format(threshold2))
+    osci.write('TRIGger:A:SETHold:DATa:THReshold {}'.format(threshold1))
     muonesIniciales = int(osci.query('ACQuire:NUMACq?'))
-    time.sleep(tiempototal/divisiones)
+    time.sleep(tiempomedicion)
     nromuones = int(osci.query('ACQuire:NUMACq?'))- muonesIniciales
-    print(nromuones)
-    muones.append(nromuones)
-    hora = str(time.time()-tiempocero)
-    np.savetxt('barridolargo_'+dateinicial+'.csv',muones,delimiter=',')
+    matrizdedatos[index][1] = nromuones
+    np.savetxt('barridolargo_' + dateinicial + '.csv',matrizdedatos,delimiter=',')
+    print(str(nromuones) + ' coincidencias para rate {}'.format(rate))
+    print('Threshold 1: {0:.2f}'.format(threshold1))
+    print('Threshold 2: {0:.2f}'.format(threshold2))
 osci.close()
