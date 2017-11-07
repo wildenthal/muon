5c158b867dc0ab4a8eef7edb44594f77da4a2dcd
wildenthal <8503469+wildenthal@users.noreply.github.com>
2017-11-07 17:10:56 -0300

    saca graficos, agrega series
    agrega la capacidad para repetir varias veces

--- scripts/osci.py
+++ scripts/osci.py
@@ -3,37 +3,37 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import os
 rm = visa.ResourceManager('@py')
 osci = rm.open_resource(rm.list_resources()[0],read_termination='\n')
 
 sigomidiendo = True
-i=0
 
+nombrecarpeta = time.strftime("%m-%d-%H%M")
+os.mkdir(nombrecarpeta)
+nroplacas = int(sys.argv[1])
+mismascondiciones = bool(int(sys.argv[2]))
+print(mismascondiciones)
+n = int(sys.argv[3])
 while sigomidiendo:
-    nroplacas = int(input('Placa 1 o 2? Para ambas ingrese 0: '))
+    #nroplacas = int(input('Placa 1 o 2? Para ambas ingrese 0: '))
     placasAbarrer = [1,2] if nroplacas == 0 else [nroplacas]
-    nombres = []
-    nombres.append(input('Nombre de la medicion 1? '))
-    if nroplacas == 0:
-        nombres.append(input('Nombre de la medicion 2? '))
-    else:
-        nombres.append(nombres[0])
-    listacolores = ['red','blue','brown','black','gold','silver','olive','orange','tomy']
+    color = ['tomy']
     arraycondiciones = [0,0]
     if nroplacas == 0:
         arraycondiciones[0] = [float(num) for num in input('Thresholds superior, inferior, nro datos, tiempo por medición para la placa 1 separados por espacios: ').split()] #.1 .2 25 60
-        if input('Mismas condiciones para placa 2? y/n: ')=='y': 
+        if mismascondiciones == True: #input('Mismas condiciones para placa 2? y/n: ')=='y': 
             arraycondiciones[1] = arraycondiciones[0]
         else:
             arraycondiciones[1] = [float(num) for num in input('Bueno dale... vos sabés qué hacer: ').split()]
     else:
         arraycondiciones[0] = [float(num) for num in input('Thresholds superior, inferior, nro datos, tiempo por medición para la placa {} separados por espacios: '.format(nroplacas)).split()]
         arraycondiciones[1] = arraycondiciones[0]
-
+    #n = int(input('¿Cuántas veces repito la medición?'))
     input("Chequeemos comunicacion: {}. Presione enter para continuar".format(osci.query('*IDN?')))
     print('Hora de detectar muones. Que la fuerza electrodébil te acompanie.')
-
-    for placa in placasAbarrer:
+    placasAbarrertotal = [item[j] for item in [placasAbarrer] for i in range(n) for j in range(len(placasAbarrer))]
+    for placa in placasAbarrertotal:
         osci.write('trigger:a:edge:slope fall; source ch{}'.format(placa))
         listathresholds = np.linspace(arraycondiciones[placa-1][0],arraycondiciones[placa-1][1],arraycondiciones[placa-1][2])
         eventos = np.zeros(len(listathresholds))
@@ -45,20 +45,8 @@ while sigomidiendo:
             time.sleep(segundos)
             mediciones = osci.query('acquire:numacq?')
             eventos[index] = int(mediciones)/segundos*60
-        np.savetxt('segplacafecha_{}_{}_{}.csv'.format(int(segundos),placa,time.strftime("%m-%d-%H%M")),np.transpose([listathresholds,eventos]),delimiter=',')
-        plt.scatter(-1*listathresholds,eventos,label=nombres[placa-1],color=listacolores[i])
-        plt.xlabel('Threshold (logV)')
-        plt.ylabel('Número de eventos (log)')
-        i+=1
+        np.savetxt(nombrecarpeta + '/segplacafecha_{}_{}_{}.csv'.format(int(segundos),placa,time.strftime("%m-%d-%H%M%s")),np.transpose([listathresholds,eventos]),delimiter=',')
     sigomidiendo = int(input('Ingrese 0 para finalizar o cualquier otra número si quiere seguir explotándome. '))
-plt.grid()
-plt.legend()
-plt.title(time.strftime("%d de %b"))
-plt.savefig(time.strftime("%d_de_%b_lin")+'.jpg',dpi=300)
-plt.xscale('log')
-plt.yscale('log')
-plt.savefig(time.strftime("%d_de_%b_log")+'.jpg',dpi=300)
-plt.show()
 input('noooo')
 osci.close()
 
