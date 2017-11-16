2005a9e3af49c049625a3cc32c4786268c740fce
wildenthal <8503469+wildenthal@users.noreply.github.com>
2017-11-16 10:14:32 -0300

    chequea comunicacion - sin cambios?

--- scripts/commcheck.py
+++ scripts/commcheck.py
@@ -1,4 +1,8 @@
 import visa
+import time
 rm = visa.ResourceManager('@py')
 osci = rm.open_resource(rm.list_resources()[0],read_termination = '\n')
+cero = time.time()
 print(osci.query('*IDN?'))
+print(time.time()-cero)
+osci.close()
