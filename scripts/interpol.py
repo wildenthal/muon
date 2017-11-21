7971e5f18962dc9d8906cc71556b1e2726b0ead1
wildenthal <8503469+wildenthal@users.noreply.github.com>
2017-11-21 16:54:02 -0300

    crea una funcion que puede ser llamada
    ahora trabaja con su carpeta interpol para devolverte el threshold correspondiente a un rate

--- scripts/interpol.py
+++ scripts/interpol.py
@@ -1,22 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-listadedatos = list(os.walk("./interpol"))[0][2]
-pmt1 = np.loadtxt("./interpol/"+listadedatos[0],delimiter=',')
-pmt2 = np.loadtxt("./interpol/"+listadedatos[1],delimiter=',')
-#plt.plot(-pmt1[:,0],pmt1[:,1],'-o',label='datos')
-def fitFunc(t, a, b, c):
-    return a*np.exp(-b*t) + c
-t = np.linspace(-pmt1[:,0][0],-pmt1[:,0][len(-pmt1[:,0])-1],50)
-temp = fitFunc(t, 2.5, 1.3, 0.5)
-noisy = temp + 0.25*np.random.normal(size=len(temp))
+def threshold(rate):
+    #ordena alfabeticamente los archivos del directorio
+    listadedatos = sorted(list(os.walk("./interpol"))[0][2])
 
+    #los divide en las dos series
+    nroarchivos = len(listadedatos)
+    cantidadporserie = nroarchivos//2
 
-#fitParams, fitCovariances = curve_fit(fitFunc, -pmt1[:,0],noisy)
+    listadedatos1 = listadedatos[0:cantidadporserie]
+    listadedatos2 = listadedatos[cantidadporserie + 1:nroarchivos]
 
-plt.plot(t, fitFunc(t, fitParams[0], fitParams[1], fitParams[2]))
-plt.show()
-input()
+    #carga las dos series de archivos sumandolas
+    datos1 = np.loadtxt("./interpol/" + listadedatos1.pop(0),delimiter=',')
+    for datito in listadedatos1:
+        datos1 = [x + y for x,y in zip(datos1,np.loadtxt("./interpol/" + datito,delimiter=','))]
+
+    datos2 = np.loadtxt("./interpol/" + listadedatos2.pop(0),delimiter=',')
+    for datito in listadedatos2:
+        datos2 = [x + y for x,y in zip(datos2,np.loadtxt("./interpol/" + datito,delimiter=','))]
+    datos = [np.matrix(datos1)/cantidadporserie,np.matrix(datos2)/cantidadporserie]
+
+    #los divide por la cantidad de archivos por serie
+    datos = [np.matrix(datos1)/cantidadporserie,np.matrix(datos2)/cantidadporserie]
+
+    #el carozo del durazno
+    thresholds = []
+    r = rate*60
+    for dato in datos:
+        for fila in range(len(dato)):
+            r1 = dato[fila,1]
+            if r < r1:
+                pass
+            else:
+                r0 = dato[fila-1,1]
+                t1 = dato[fila,0]
+                t0 = dato[fila-1,0]
+                T = t1 + (r - r1)/(r0- r1) * (t0 - t1)
+                thresholds.append(T)
+                break
+    return thresholds
