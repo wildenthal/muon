a2704d4229fa3839b731f1a216b3af96d4f682a2
wildenthal <8503469+wildenthal@users.noreply.github.com>
2017-11-21 16:48:24 -0300

    agregamos cambios de escala automaticos
    a calibracion_individual también, en el commit de esta fecha para ese archivo. No lo mencionamos en el commit porque somos unos desorganizados de mier-

--- scripts/calibracion_coincidencias.py
+++ scripts/calibracion_coincidencias.py
@@ -3,9 +3,11 @@ from time import strftime,sleep
 import numpy as np
 import sys
 import os
+from interpol import threshold
+from escalafunc import escalafunc
 
 #El programa debe ser llamado de la siguiente manera:
-# >>sudo python3 barridolargo.py rateminimo ratemaximo nropuntos tiempoporpunto repeticiones (
+# >>python3 barridolargo.py rateminimo ratemaximo nropuntos tiempoporpunto repeticiones (
 #donde el tiempo esta medido en segundos
 #rate debe ser mayor que 22
 
@@ -14,46 +16,38 @@ osci = rm.open_resource(rm.list_resources()[0],read_termination='\n')
 
 #Parametros de nuestra medicion
 nropuntos = int(sys.argv[3])
-ratelist = np.logspace(float(sys.argv[1]), float(sys.argv[2]), int(sys.argv[3]))
+ratelist = np.linspace(float(sys.argv[1]), float(sys.argv[2]), int(sys.argv[3]))
 tiempomedicion = float(sys.argv[4]) #Cuanto tiempo quiero barrer cada threshold
 repeticiones = int(sys.argv[5])
 
-#Funcion que calcula el treshold para cada PMT en funcion del rate deseado
-def thresholds(rate):
-    y2 = 0
-    A2 = 242.7012
-    R2 = 51.23416
-    y1 = 0.78340
-    A1 = 159.0289
-    R1 = 83.3262
-    threshold1 = np.log((rate + y1)/A1)/R1 
-    threshold2 = np.log((rate + y2)/A2)/R2 
-    return threshold1, threshold2
-
-print(np.transpose(np.matrix(ratelist)))
-print(thresholds(30))
+ratestring = ', 	'.join(map(str,ratelist.tolist()))
+print('Rates a barrer: ' + ratestring)
 
 #Crea csv para guardar
 matrizdedatos = np.transpose([ratelist,np.zeros(nropuntos)])
 
 #Hace el barrido
-nombrecarpeta = "coincidencias_"+strftime("%H_%M")
+nombrecarpeta = "coincidencias_"+strftime("%y.%m.%d_%H.%M")
 os.mkdir(nombrecarpeta)
 
 for j in range(repeticiones):
     dateinicial = strftime("%y.%m.%d_%H.%M.%s")
 
     for index,rate in enumerate(ratelist):
-        threshold1, threshold2 = thresholds(rate)
-        osci.write('TRIGger:A:SETHold:CLOCk:THReshold {}'.format(threshold2))
-        osci.write('TRIGger:A:SETHold:DATa:THReshold {}'.format(threshold1))
-        print('vamos bien')
+        threshold1, threshold2 = threshold(rate)
+        escala1 = escalafunc(threshold1)
+        escala2 = escalafunc(threshold2)
+        osci.write('ch1:scale {}; :ch2:scale {}'.format(escala1,escala2))
+        osci.write('TRIGger:A:SETHold:CLOCk:THReshold {}'.format(threshold1))
+        osci.write('TRIGger:A:SETHold:DATa:THReshold {}'.format(threshold2))
+        print('Estamos configurando la siguiente medición. Por favor espere...')
+        sleep(1)
         muonesIniciales = int(osci.query('ACQuire:NUMACq?'))
+        print('{} eventos iniciales'.format(muonesIniciales))
+        print('Para el rate {} seteamos los thresholds en {}mV para el canal 1 y {}mV para el 2'.format(rate,int(threshold1*-1000),int(threshold2*-1000)))
         sleep(tiempomedicion)
-        nromuones = int(osci.query('ACQuire:NUMACq?'))- muonesIniciales
+        nromuones = int(osci.query('ACQuire:NUMACq?')) - muonesIniciales
         matrizdedatos[index][1] = nromuones
         np.savetxt(nombrecarpeta + '/coinc' + dateinicial + '.csv',matrizdedatos,delimiter=',')
-        print(str(nromuones) + ' coincidencias para rate {}'.format(rate))
-        print('Threshold 1: {}'.format(threshold1))
-        print('Threshold 2: {}'.format(threshold2))
+        print(str(nromuones) + ' coincidencias')
 osci.close()
