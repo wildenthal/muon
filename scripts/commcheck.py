3a83f950fe1b60601668241e9c0b72549188ae26
wildenthal <8503469+wildenthal@users.noreply.github.com>
2017-11-21 16:49:43 -0300

    sí había habido un cambio en el commit anterior
    agregamos un test de velocidad; ahora lo sacamos y agregamos un loop con cambios de escala para ver qué pasaba con el número de adquisiciones y cuánto tardaba en reiniciarse

--- scripts/commcheck.py
+++ scripts/commcheck.py
@@ -3,6 +3,9 @@ import time
 rm = visa.ResourceManager('@py')
 osci = rm.open_resource(rm.list_resources()[0],read_termination = '\n')
 cero = time.time()
-print(osci.query('*IDN?'))
-print(time.time()-cero)
+osci.query('*IDN?')
+while(True):
+    input()
+    osci.write('ch1:scale {}; :ch2:scale {}'.format(escala1,escala2))
+    print(osci.query('ACQuire:NUMACq?'))
 osci.close()
