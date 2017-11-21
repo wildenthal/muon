b0d2cc92d517f6c8428de001ba1edd825a145504
wildenthal <8503469+wildenthal@users.noreply.github.com>
2017-11-21 16:50:06 -0300

    ahora funciona normalmente (antes era un engendro que no andaba)

--- scripts/graficator.py
+++ scripts/graficator.py
@@ -5,13 +5,18 @@ import sys
 import matplotlib.pyplot as plt
 
 listadedatos = list(os.walk("./graficar/"))[0][2]
+
 print(listadedatos)
-for grafico in listadedatos:
+suma = []
+for index,grafico in enumerate(listadedatos):
     dato = np.loadtxt("./graficar/" + grafico,delimiter=',')
-    etiqueta=input("Leyenda para {}? ".format(grafico))
-    colorcito=input("Color? ")
-    escala = float(input("Multiplico por? "))
-    plt.plot(-dato[:,0],dato[:,1]*escala,'-o',label=etiqueta,color=colorcito,)
+    #etiqueta=input("Leyenda para {}? ".format(grafico))
+    #colorcito=input("Color? ")
+    #escala = float(input("Multiplico por? "))
+    suma.append(dato[0,1])
+print(suma)
+sys.exit()
+plt.scatter(index,suma)#,'-o',label=etiqueta,color=colorcito,) #-dato[:,0]
 plt.legend()
 plt.grid()
 nombre = input('nombre? ')
