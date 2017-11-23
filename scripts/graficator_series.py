a4a804743c9046e568626e4f151993ef37c67c3a
wildenthal <8503469+wildenthal@users.noreply.github.com>
2017-11-23 15:52:30 -0300

    modifica graficator_series para series múltiples, corrige file management
    ahora se puede graficar n series mientras sean todas de la misma cantidad de archivos (que sea variable va a ser la próxima innovación). También no andaba lo que había hecho para que se centrase en el directorio del archivo, ahora sí

--- scripts/graficator_series.py
+++ scripts/graficator_series.py
@@ -5,30 +5,31 @@ import sys
 import matplotlib.pyplot as plt
 
 #File management
-os.chdir(os.path.dirname(sys.argv[0])) #setea bien el directorio
+os.chdir(os.path.dirname(os.path.abspath(__file__))) #setea bien el directorio
 pathname = "../datos_temp/"+time.strftime("%d%b")+"/"
-os.mkdir(pathname) if not(os.path.isdir(pathname)) else pass
+os.mkdir(pathname) if not(os.path.isdir(pathname)) else print('ok')
 
 #Crea lista de datos de directorio
 listadedatos = sorted(list(os.walk("./graficar/"))[0][2])
 nroarchivos = len(listadedatos)
 cantidadporserie = nroarchivos//int(sys.argv[1]) #hay que pasarle 1 o 2
-
+'''
 #Carga primer serie de datos
 listadedatos1 = listadedatos[0:cantidadporserie]
 datos1 = np.loadtxt("./graficar/" + listadedatos1.pop(0),delimiter=',')
 for datito in listadedatos1:
     datos1 = [x + y for x,y in zip(datos1,np.loadtxt("./graficar/" + datito,delimiter=','))]
+'''
 
-#Carga segunda serie de datos (falla si se le paso cantidadporserie = 1)
-try:
-    listadedatos2 = listadedatos[cantidadporserie + 1:nroarchivos]
+#Carga las series de datos
+datos=[]
+for nroserie in range(int(sys.argv[1])):
+    listadedatos2 = listadedatos[nroserie*cantidadporserie:(nroserie+1)*cantidadporserie]#[cantidadporserie + 1:nroarchivos]
+    print(listadedatos2)
     datos2 = np.loadtxt("./graficar/" + listadedatos2.pop(0),delimiter=',')
     for datito in listadedatos2:
         datos2 = [x + y for x,y in zip(datos2,np.loadtxt("./graficar/" + datito,delimiter=','))]
-    datos = [np.matrix(datos1)/cantidadporserie,np.matrix(datos2)/cantidadporserie]
-except IndexError:
-    datos = [np.matrix(datos1)/cantidadporserie]
+    datos.append(np.matrix(datos2)/cantidadporserie)#= [np.matrix(datos1)/cantidadporserie,np.matrix(datos2)/cantidadporserie]
 
 #Grafica formatea y guarda
 for index,dato in enumerate(datos):
