f335e60cd9d46c570ea31abf80f097716b39a98a
wildenthal <8503469+wildenthal@users.noreply.github.com>
2017-11-28 15:30:19 -0300

    corrige graficator.py para que compile pero no sé qué hace ahora

--- scripts/graficator.py
+++ scripts/graficator.py
@@ -2,11 +2,13 @@ import numpy as np
 import os
 import sys
 import matplotlib.pyplot as plt
+import time
 
 #File management
-os.chdir(os.path.dirname(sys.argv[0])) #setea bien el directorio
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  #setea bien el directorio
 pathname = "../datos_temp/"+time.strftime("%d%b")+"/"
-os.mkdir(pathname) if not(os.path.isdir(pathname)) else pass
+if not(os.path.isdir(pathname)):
+    os.mkdir(pathname)
 
 listadedatos = list(os.walk("./graficar/"))[0][2]
 
@@ -14,13 +16,13 @@ print(listadedatos)
 suma = []
 for index,grafico in enumerate(listadedatos):
     dato = np.loadtxt("./graficar/" + grafico,delimiter=',')
-    #etiqueta=input("Leyenda para {}? ".format(grafico))
-    #colorcito=input("Color? ")
-    #escala = float(input("Multiplico por? "))
+    etiqueta=input("Leyenda para {}? ".format(grafico))
+    colorcito=input("Color? ")
+    escala = float(input("Multiplico por? "))
     suma.append(dato[0,1])
 print(suma)
 sys.exit()
-plt.scatter(index,suma)#,'-o',label=etiqueta,color=colorcito,) #-dato[:,0]
+plt.scatter(index,suma,'-o',label=etiqueta,color=colorcito,) #-dato[:,0]
 plt.legend()
 plt.grid()
 nombre = input('nombre? ')
@@ -29,3 +31,4 @@ plt.xlabel("Thresholds")
 plt.ylabel("Rate")
 plt.savefig(pathname + nombre + ".jpg",dpi=300)
 plt.show()
+input()
